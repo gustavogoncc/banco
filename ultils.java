eed4ea1a01912a33a3d60e96ec5ae3601d262c08
José Gustavo G. da Silva <105744484+gustavogoncc@users.noreply.github.com>
2023-07-25 20:58:18 -0300

    Create ultils.java

---
+++ ultils.java
@@ -0,0 +1,14 @@
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class ultils {
+	
+	static NumberFormat formatandoValores = new DecimalFormat("R$ #,##0.00");
+	
+	public static String doubleToString(Double valor) {
+		return formatandoValores.format(valor);
+		
+	}
+
+}
